199f429f6b4971c7b468521efcaa5c54d18fa0be
Mário Antunes <mario.antunes@ua.pt>
2020-12-02 15:27:51 +0000

    Added a setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+import pathlib
 
-setup(name='uts',
-      version='1.0',
-      description='Python Distribution Utilities',
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+      name='uts',
+      version='0.1',
+      description='uts Algorithms',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Mário antunes',
       author_email='mariolpantunes@gmail.com',
-      url=https://github.com/mariolpantunes/uts',
-      packages=['distutils', 'distutils.command'],
-     )
+      url='https://github.com/mariolpantunes/uts',
+      package_dir={'': 'uts'},
+      packages=find_packages(where='uts'),
+      install_requires=['numpy>=1.19.4']
+)
