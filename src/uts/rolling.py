458c7271aac6ce974c7f9785dcc0669ec1d82caa
Mario Antunes <mariolpantunes@gmail.com>
2023-09-08 13:27:14 +0100

    Added more methods to rolling

--- src/uts/rolling.py
+++ src/uts/rolling.py
@@ -6,12 +6,34 @@ __email__ = 'mariolpantunes@gmail.com'
 __status__ = 'Development'
 
 
+import math
 import numpy as np
+
+
+def _compensated_addition(sum_current: float, addend: float, comp:float) -> (float, float):
+    """
+    Compensated addition using Kahan (1965) summation algorithm.
+
+    Args:
+        sum_current (float): sum calculated so far
+        addend (float): value to be added to 'sum'
+        comp (float): accumulated numeric error so far
+    
+    Returns:
+        (float, float): new values for sum_current and comp
+    """
+    
+    addend -= comp
+    sum_new = sum_current + addend
+    comp = (sum_new - sum_current) - addend
+    sum_current = sum_new
+
+    return sum_current, comp
 
 
 def num_obs(values: np.ndarray, width_before: float, width_after: float) -> np.ndarray:
     """
-    Rolling number of observation values
+    Rolling number of observation values.
 
     Args:
         values (np.ndarray): array of time series values
@@ -45,4 +67,149 @@ def num_obs(values: np.ndarray, width_before: float, width_after: float) -> np.n
         # Number of observations is equal to length of window
         rv[i] = np.array([values[i][0], (right - left + 1)])
 
+    return rv
+
+
+def sum(values: np.ndarray, width_before: float, width_after: float) -> float:
+    """
+    Rolling sum of observation values.
+
+    Args:
+        values (np.ndarray): array of time series values
+        width_before (float): (non-negative) width of rolling window before t_i
+        width_after (float): (non-negative) width of rolling window after t_i
+    
+    Returns:
+        np.ndarray: array with time series values
+    """
+
+    n = len(values)
+    # Trivial case
+    if n == 0:
+        return np.array([])
+    
+    # Initialize output
+    rv = np.empty([n, 2])
+
+    left = 0
+    right = -1
+
+    roll_sum = 0.0
+
+    for i in range(0, n):
+        # Expand window on the right
+        while ((right < (n - 1)) and (values[(right + 1)][0] <= values[i][0] + width_after)):
+            right += 1
+            roll_sum += values[right][1]
+        
+        # Shrink window on the left
+        while ((left < n) and (values[left][0] <= values[i][0] - width_before)):
+            roll_sum -= values[left][1]
+            left+=1
+        
+        # Update rolling sum
+        rv[i] = np.array([values[i][0], roll_sum])
+
+    return rv
+
+
+def sum_stable(values: np.ndarray, width_before: float, width_after: float) -> np.ndarray:
+    """
+    Same as rolling_sum, but use Kahan (1965) summation algorithm to reduce numerical error.
+
+    Args:
+        values (np.ndarray): array of time series values
+        width_before (float): (non-negative) width of rolling window before t_i
+        width_after (float): (non-negative) width of rolling window after t_i
+    
+    Returns:
+        np.ndarray: array with time series values
+    """
+
+    n = len(values)
+    # Trivial case
+    if n == 0:
+        return np.array([])
+    
+    # Initialize output
+    rv = np.empty([n, 2])
+
+    left = 0
+    right = -1
+
+    roll_sum = 0.0
+    comp = 0.0
+
+    for i in range(0, n):
+        # Expand window on the right
+        while ((right < (n - 1)) and (values[(right + 1)][0] <= values[i][0] + width_after)):
+            right += 1
+            roll_sum, comp = _compensated_addition(roll_sum, values[right][1], comp)
+
+        # Shrink window on the left
+        while ((left < n) and (values[left][0] <= values[i][0] - width_before)):
+            roll_sum, comp = _compensated_addition(roll_sum, -values[left][1], comp)
+            left+=1
+        
+        # Update rolling sum
+        rv[i] = np.array([values[i][0], roll_sum])
+
+    return rv
+
+
+def product(values: np.ndarray, width_before: float, width_after: float,
+eps:float = np.finfo(np.float32).eps) -> np.ndarray:
+    """
+    Rolling product of observation values.
+
+    Args:
+        values (np.ndarray): array of time series values
+        width_before (float): (non-negative) width of rolling window before t_i
+        width_after (float): (non-negative) width of rolling window after t_i
+        eps (float): epsilon (default: np.finfo(np.float32).eps)
+    
+    Returns:
+        np.ndarray: array with time series values
+    """
+
+    n = len(values)
+    # Trivial case
+    if n == 0:
+        return np.array([])
+    
+    # Initialize output
+    rv = np.empty([n, 2])
+
+    left = 0
+    right = -1
+    most_recent_zero = -1
+    roll_product = 1.0
+
+    for i in range(0, n):
+        # Expand window on the right
+        while ((right < (n - 1)) and (values[(right + 1)][0] <= values[i][0] + width_after)):
+            right += 1
+            roll_product *= values[right][1]
+
+            # Save position of most recent zero
+            if (math.fabs(0.0 - values[right][1]) <= eps):
+                most_recent_zero = right
+        
+        # Shrink window on the left
+        while ((left < n) and (values[left][0] <= values[i][0] - width_before)):
+            # Don't need to update rolling product if zero drops out, because calculated from scratch below
+            if (math.fabs(0.0-values[left][1]) > eps):
+                roll_product /= values[left][1]
+            left += 1
+        
+        # Update rolling product
+        # -) need to calculate from scratch in case a zero dropped out of the window
+        if ((roll_product == 0.0) and (most_recent_zero < left)):
+            roll_product = 1.0
+            #for (int pos=left; pos <= right; pos++)
+            for pos in range(left, right+1):
+                roll_product *= values[pos][1]
+        
+        rv[i] = np.array([values[i][0], roll_product])
+    
     return rv
--- test/test_rolling.py
+++ test/test_rolling.py
@@ -6,10 +6,34 @@ from uts import rolling
 
 
 class Test_Rolling(unittest.TestCase):
-    def test_ema_next(self):
+    def test_num_obs(self):
         values = np.array([[0.0, 0.0], [1.0, 2.0],
         [1.2, 4.0], [2.3, 6], [2.9, 8], [5, 10]])
         result = rolling.num_obs(values, 2.5, 1.0)
         desired = np.array([[0.0, 2.0], [1.0, 3.0],
         [1.2, 3.0], [2.3, 5.0], [2.9, 4.0], [5, 2.0]])
+        npt.assert_almost_equal(result, desired, decimal=2)
+    
+    def test_sum(self):
+        values = np.array([[0.0, 0.0], [1.0, 2.0],
+        [1.2, 4.0], [2.3, 6], [2.9, 8], [5, 10]])
+        result = rolling.sum(values, 2.5, 1.0)
+        desired = np.array([[0.0, 2.0], [1.0, 6.0],
+        [1.2, 6.0], [2.3, 20.0], [2.9, 20.0], [5, 18.0]])
+        npt.assert_almost_equal(result, desired, decimal=2)
+    
+    def test_sum_stable(self):
+        values = np.array([[0.0, 0.0], [1.0, 2.0],
+        [1.2, 4.0], [2.3, 6], [2.9, 8], [5, 10]])
+        result = rolling.sum_stable(values, 2.5, 1.0)
+        desired = np.array([[0.0, 2.0], [1.0, 6.0],
+        [1.2, 6.0], [2.3, 20.0], [2.9, 20.0], [5, 18.0]])
+        npt.assert_almost_equal(result, desired, decimal=2)
+    
+    def test_product(self):
+        values = np.array([[0.0, 0.0], [1.0, 2.0],
+        [1.2, 4.0], [2.3, 6], [2.9, 8], [5, 10]])
+        result = rolling.product(values, 2.5, 1.0, eps=1e-10)
+        desired = np.array([[0.0, 0.0], [1.0, 0.0],
+        [1.2, 0.0], [2.3, 0.0], [2.9, 384.0], [5, 80.0]])
         npt.assert_almost_equal(result, desired, decimal=2)
